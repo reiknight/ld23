a6b4ecfa3b7ea25270e16a87ef0acc1ce9b39cf1
unknown <Hctor@hector-desktop.(none)>
2012-04-21 02:37:17 +0200

    Initial commit

---
+++ src/ld23/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ld23;
+
+/**
+ *
+ * @author Héctor
+ */
+public class Main {
+    
+}
