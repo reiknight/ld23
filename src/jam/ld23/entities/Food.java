d65c288f818a2a13e1f072b1c67f7939ee2d1175
Reik Val <guardian_de_las_sombras@hotmail.com>
2012-04-23 00:54:07 +0200

    Food explosion

--- src/jam/ld23/entities/Food.java
+++ src/jam/ld23/entities/Food.java
@@ -18,28 +18,35 @@ public class Food extends Sprite {
     private static int idSmall = 0;
     private static Random rand;
     private float rotatingSpeed;
-    private boolean collisionable;
-    private boolean needTimer;
+    private Vector2f direction;
+    private int type;
 
     //Initialize block for the two constructors
-    {
+    static {
         rand = new Random();
+    }
+    
+    {
         rotatingSpeed = (rand.nextInt(20) - 10) * .01F;
         setPosition(new Vector2f(C.SCREEN_WIDTH, rand.nextFloat() * C.SCREEN_HEIGHT));
         group = C.Groups.FOOD.name;
-        collisionable = true;
-        needTimer = true;
     }
 
     public Food() {
-        String texture = C.Textures.valueOf(C.Entities.FOOD_NORMAL.name + rand.nextInt(3)).name;
+        type = rand.nextInt(3);
+        String texture = C.Textures.valueOf(C.Entities.FOOD_NORMAL.name + type).name;
         size = Size.NORMAL;
         name = C.Entities.FOOD_NORMAL.name + idNormal++;
         this.setTexture(texture);
     }
 
     public Food(Size size) {
-        String texture = C.Textures.valueOf("FOOD_" + size + "_" + rand.nextInt(3)).name;
+        this(size,rand.nextInt(3));
+    }
+    
+    public Food(Size size, int type) {
+        this.type = type;
+        String texture = C.Textures.valueOf("FOOD_" + size + "_" + type).name;
         switch (size) {
             case SMALL:
                 name = C.Entities.FOOD_SMALL.name + idSmall++;
@@ -54,6 +61,8 @@ public class Food extends Sprite {
         this.size = size;
         this.setTexture(texture);
     }
+    
+    
 
     @Override
     public void update(GameContainer gc, int delta) {
@@ -78,14 +87,40 @@ public class Food extends Sprite {
             Bullet bullet = (Bullet) bullets.get(i);
             if (pm.testCollisionsEntity(this, bullet)) {
                 em.removeEntity(bullet.getName());
+                em.removeEntity(this.getName());
+                spawnEntities();
             }
         }
 
+        //Collision with the player
         Player player = (Player) em.getEntity(C.Entities.PLAYER.name);
-        if (pm.testCollisionsEntity(this, player) & collisionable) {
+        if (pm.testCollisionsEntity(this, player)) {
             player.setPosition(new Vector2f(x - player.getWidth(), player.getY()));
         }
 
     }
 
+    private void spawnEntities() {
+        EntityManager em = EntityManager.getInstance();
+        Food f1,f2;
+        switch(size) {
+            case BIG:
+                f1 = new Food(Size.NORMAL,type);
+                f1.setPosition(new Vector2f(getX(),getY()-10));
+                em.addFutureEntity(f1.getName(),f1);
+                f2 = new Food(Size.NORMAL,type);
+                f2.setPosition(new Vector2f(getX(),getY()+10));
+                em.addFutureEntity(f2.getName(),f2);
+                break;
+            case NORMAL:
+                f1 = new Food(Size.SMALL,type);
+                f1.setPosition(new Vector2f(getX(),getY()-10));
+                em.addFutureEntity(f1.getName(),f1);
+                f2 = new Food(Size.SMALL,type);
+                f2.setPosition(new Vector2f(getX(),getY()+10));
+                em.addFutureEntity(f2.getName(),f2);
+                break;
+        }
+        
+    }
 }
