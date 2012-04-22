ea7f09e81c619f843072507c2533b1cedc6f63d0
ultrayoshi <david.morcillo@gmail.com>
2012-04-22 13:17:56 +0200

    WIP: constants unique class

---
+++ src/jam/ld23/game/C.java
@@ -0,0 +1,13 @@
+package jam.ld23.game;
+
+public class C {
+    public static enum Events = {
+        CLOSE_WINDOW,
+        MOVE_LEFT,
+        MOVE_RIGHT,
+        MOVE_UP,
+        MOVE_DOWN,
+        FIRE,
+        CROSSHAIR_MOVED
+    }
+}
