83fef3f733e9d9dc7fd7f975051ca3866d4daf4a
Reik Val <guardian_de_las_sombras@hotmail.com>
2012-04-22 13:24:52 +0200

    Enums

--- src/jam/ld23/game/C.java
+++ src/jam/ld23/game/C.java
@@ -1,13 +1,28 @@
 package jam.ld23.game;
 
 public class C {
-    public static enum Events = {
+    public static enum Events {
         CLOSE_WINDOW,
         MOVE_LEFT,
         MOVE_RIGHT,
         MOVE_UP,
         MOVE_DOWN,
         FIRE,
-        CROSSHAIR_MOVED
+        CROSSHAIR_MOVED;
+    }
+    
+    public static enum Textures {
+        ENEMY("resources/enemy.png");
+        
+        private String path;
+
+        private Textures(String path) {
+            this.path = path;
+        }
+        
+        @Override
+        public String toString() {
+            return path;
+        }
     }
 }
