718786275f4ad3ce441dad1855b546b390babec2
Reik Val <guardian_de_las_sombras@hotmail.com>
2012-04-23 00:01:52 +0200

    Collisions with food

--- src/jam/ld23/entities/Food.java
+++ src/jam/ld23/entities/Food.java
@@ -6,11 +6,12 @@ import jam.ld23.physics.PhysicsManager;
 import jam.ld23.sounds.SoundManager;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Timer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Food extends Sprite {
-    
+
     private Size size;
     private float speed = (float) 0.1;
     private static int idBig = 0;
@@ -18,39 +19,43 @@ public class Food extends Sprite {
     private static int idSmall = 0;
     private static Random rand;
     private float rotatingSpeed;
+    private boolean collisionable;
+    private boolean needTimer;
 
     //Initialize block for the two constructors
     {
         rand = new Random();
-        rotatingSpeed = (rand.nextInt(20)-10) * .01F;
-        setPosition(new Vector2f(C.SCREEN_WIDTH, rand.nextFloat()*C.SCREEN_HEIGHT));
+        rotatingSpeed = (rand.nextInt(20) - 10) * .01F;
+        setPosition(new Vector2f(C.SCREEN_WIDTH, rand.nextFloat() * C.SCREEN_HEIGHT));
         group = C.Groups.FOOD.name;
+        collisionable = true;
+        needTimer = true;
     }
-    
+
     public Food() {
-        String texture = C.Textures.valueOf(C.Entities.FOOD_NORMAL.name+rand.nextInt(3)).name;
+        String texture = C.Textures.valueOf(C.Entities.FOOD_NORMAL.name + rand.nextInt(3)).name;
         size = Size.NORMAL;
         name = C.Entities.FOOD_NORMAL.name + idNormal++;
         this.setTexture(texture);
     }
-    
+
     public Food(Size size) {
-        String texture = C.Textures.valueOf("FOOD_"+size+"_"+rand.nextInt(3)).name;
-        switch(size) {
+        String texture = C.Textures.valueOf("FOOD_" + size + "_" + rand.nextInt(3)).name;
+        switch (size) {
             case SMALL:
                 name = C.Entities.FOOD_SMALL.name + idSmall++;
                 break;
-            case NORMAL:       
+            case NORMAL:
                 name = C.Entities.FOOD_NORMAL.name + idNormal++;
                 break;
-            case BIG:     
+            case BIG:
                 name = C.Entities.FOOD_BIG.name + idBig++;
                 break;
         }
         this.size = size;
         this.setTexture(texture);
     }
-    
+
     @Override
     public void update(GameContainer gc, int delta) {
         super.update(gc, delta);
@@ -58,31 +63,30 @@ public class Food extends Sprite {
         EventManager evm = EventManager.getInstance();
         SoundManager sm = SoundManager.getInstance();
         PhysicsManager pm = PhysicsManager.getInstance();
-        
+
         //Food movement
         float x = getX();
         float y = getY();
         x -= speed * delta;
-        setPosition(new Vector2f(x, y));
-        
+        Vector2f v;
+        setPosition(v = new Vector2f(x, y));
+
         image.rotate(rotatingSpeed);
-        
+
         //Testing Collisions with Bullets
         ArrayList<Entity> bullets = em.getEntityGroup(C.Groups.BULLETS.name);
-        for(int i = 0; i < bullets.size(); i++) {
+        for (int i = 0; i < bullets.size(); i++) {
             Bullet bullet = (Bullet) bullets.get(i);
-            if(pm.testCollisionsEntity(this, bullet)) {
+            if (pm.testCollisionsEntity(this, bullet)) {
                 em.removeEntity(bullet.getName());
             }
         }
-        
+
         Player player = (Player) em.getEntity(C.Entities.PLAYER.name);
-        if(pm.testCollisionsEntity(this,player)) {
-            player.setX(this.getX());
-            player.setY(this.getY());
+        if (pm.testCollisionsEntity(this, player) & collisionable) {
+            player.setPosition(new Vector2f(x - player.getWidth(), player.getY()));
         }
+
     }
-    
-    
 
 }
