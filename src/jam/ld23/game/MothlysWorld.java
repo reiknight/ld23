2c93118aa1e4c0a68e725e5706afdf0397fa246a
Reik Val <guardian_de_las_sombras@hotmail.com>
2012-04-23 17:38:48 +0200

    Buah

--- src/jam/ld23/game/MothlysWorld.java
+++ src/jam/ld23/game/MothlysWorld.java
@@ -5,7 +5,6 @@
 package jam.ld23.game;
 
 import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.Game;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
